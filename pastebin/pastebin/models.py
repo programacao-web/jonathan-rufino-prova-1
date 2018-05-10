ac4fe374209d4796fd1888f62f8d627520912688
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2018-05-10 17:24:21 -0300

    Respostas 3, 4, 5

--- pastebin/pastebin/models.py
+++ pastebin/pastebin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Paste(models.Model):
+    paste_title = models.CharField(max_length=50)
+    paste_language = models.CharField(max_length=25)
+    paste_data = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.paste_title
